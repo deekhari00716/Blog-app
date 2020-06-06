b0ec6676a391f0fc13f1ffcf3f1185232397e22e
deekshith <deekhsri00716@gmail.com>
2020-06-06 19:09:45 +0530

    my first commit

---
+++ theblog/forms.py
@@ -0,0 +1,27 @@
+from .models import Post
+from django import forms
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'author', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'author': forms.Select(attrs={'class':'form-control'}),
+            'body': forms.Textarea(attrs={'class':'form-control'}),
+
+        }
+
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'body': forms.Textarea(attrs={'class':'form-control'}),
+
+        }
---
+++ theblog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
+
+
+# def home(request):
+#     return render(request, 'home.html', {})
+
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+    ordering = ['-post_date']
+
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'add_post.html'
+    # fields = ('title', 'author', 'body')
+
+class UpdatePostView(UpdateView):
+    model = Post
+    template_name = 'update_post.html'
+    form_class = EditForm
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
